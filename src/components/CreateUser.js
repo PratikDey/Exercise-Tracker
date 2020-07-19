2c2bb5f43856311f30f45b08cd73f1345fd70615
PratikDey <56970651+PratikDey@users.noreply.github.com>
2020-07-19 20:41:04 +0530

    issue with edit route

---
+++ src/components/CreateUser.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import axios from "axios";
+export default function CreateUser() {
+  const [username, setUsername] = useState("");
+  return (
+    <div>
+      <h3>Create New User</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newUser = { username: username };
+          console.log(newUser);
+          axios
+            .post("http://localhost:5000/users/add", newUser)
+            .then((res) => console.log(res.data));
+          setUsername("");
+        }}
+      >
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
